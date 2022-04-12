390ede9de12c1ad30034c4a6f4b42a8d701c1e40
TriciaCArt <illiannamama@gmail.com>
2022-04-12 11:06:13 -0400

    different functions

--- js/site.js
+++ js/site.js
@@ -35,6 +35,52 @@ function fizzBuzzA(fizz, buzz) {
     return fbvalues;
 }
 
+function fizzbuzzB(fizz, buzz) {
+    let fbvalues = [];
+
+    let isFizz = false;
+    let isBuzz = false;
+
+    for (let i = 1; i < 101; i++) {
+
+        isFizz = (i % fizz == 0);
+        isBuzz = (i % buzz == 0);
+
+        switch (true) {
+            case isFizz && isBuzz: {
+                fbvalues.push('FizzBuzz');
+                break;
+            }
+            case isFizz: {
+                fbvalues.push('Fizz');
+                break;
+            }
+            case isBuzz: {
+                fbvalues.push('Buzz');
+                break;
+            }
+            default: {
+                fbvalues.push(i);
+                break;
+            }
+        }
+    }
+    return fbvalues
+}
+
+// ternary function
+function fizzBuzzC(fizz, buzz) {
+    let fbvalues = [];
+
+    for (let i = 1; i < 101; i++) {
+        let value = ((i % fizz == 0 ? 'Fizz' : '') + (i % buzz == 0 ? 'Buzz' : '') || i);
+        fbvalues.push(value);
+
+    }
+
+    return fbvalues;
+}
+
 function displayValues(fbvalues) {
     let element = document.getElementById('results');
     element.innerHTML = "";
